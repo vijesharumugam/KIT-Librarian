ff822debc6595ccc52086f31e57f4837c04bcd32
vijesharumugam <vijeshvijesh2601@gmail.com>
2025-08-28 14:22:26 +0530

    back to already committed file

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
+const API_URL = process.env.REACT_APP_API_URL || 'https://kit-librarian-backend.onrender.com';
 
 const api = axios.create({
   baseURL: API_URL,
